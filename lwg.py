32da528063fc99a751cb8569689f55ca4168c9a7
mufHaQ <masbrowmess33@gmail.com>
2021-08-04 19:38:09 +0700

    add: flag for write 2D-List in different file name

--- lwg.py
+++ lwg.py
@@ -8,17 +8,19 @@ parser.add_argument("--action", dest="action", default="create", help="'create' 
 parser.add_argument("--max", dest="max", default="100", help="Max value for 2D-List")
 parser.add_argument("--length", dest="inn", default="5", help="List length")
 parser.add_argument("--append", dest="a", default="w", help="Append to new line\nDefault is 'w' | Write and overwrite data if data exists | 'a' for append")
+parser.add_argument("--file", dest="file", default="list", help="File name for 2D-List")
 args = parser.parse_args()
 
 
-mx = int(args.max)
-inn = int(args.inn)
 
 def create_list():
+    mx = int(args.max)
+    inn = int(args.inn)
     data = []
     data_inner = []
     count = 1
     vl = 1
+    file_name = args.file + '.txt'
     for _ in range(1, math.ceil(mx/inn)+1):
         for val in range(vl, mx+1):
             if count > inn:
@@ -30,7 +32,7 @@ def create_list():
         data_inner = []
         count = 1
     try:
-        with open('list.txt', args.a) as file:
+        with open(file_name, args.a) as file:
             file.write(f"{data}\n")
     except Exception as e:
         print("Error:", e)
