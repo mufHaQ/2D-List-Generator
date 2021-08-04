1727c2459c0fab5faf99e46bf74866b4272382e5
mufHaQ <masbrowmess33@gmail.com>
2021-08-04 19:12:44 +0700

    update: add terminal flags

--- lwg.py
+++ lwg.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python3
 
-import math
+import math, argparse
+from typing import Reversible
 
-def create_list(mx, gp):
+parser = argparse.ArgumentParser()
+parser.add_argument("--action", dest="action", default="create", help="'create' for create new list | 'print' for print list")
+parser.add_argument("--max", dest="max", default="100", help="Max value for 2D-List")
+parser.add_argument("--length", dest="inn", default="5", help="List length")
+parser.add_argument("--append", dest="a", default="w", help="Append to new line\nDefault is 'w' | Write and overwrite data if data exists | 'a' for append")
+args = parser.parse_args()
+
+
+mx = int(args.max)
+inn = int(args.inn)
+
+def create_list():
     data = []
     data_inner = []
     count = 1
     vl = 1
-
-    for _ in range(1, math.ceil(mx/gp)+1):
-        
+    for _ in range(1, math.ceil(mx/inn)+1):
         for val in range(vl, mx+1):
-
-            if count > gp:
+            if count > inn:
                 continue
-
             data_inner.append(val)
             count += 1
             vl = val + 1
-            
         data.append(data_inner)
         data_inner = []
         count = 1
-        
-    return data
+    try:
+        with open('list.txt', args.a) as file:
+            file.write(f"{data}\n")
+    except Exception as e:
+        print("Error:", e)
 
-data = create_list(100, 10)
-
-print(data)
-print(len(data))
+if args.action == "create":
+    create_list()
+elif args.action == "print":
+    with open('list.txt') as file:
+        print(file.read())
