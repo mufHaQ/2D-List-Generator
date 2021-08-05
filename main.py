30fd9bb919095cf0b4a685e5e54af1bdfe7a2d6a
mufHaQ <masbrowmess33@gmail.com>
2021-08-05 19:18:36 +0700

    update

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import math, argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--action", dest="action", default="write", help="'create' for create new list | 'print' for print list")
-parser.add_argument("--max", dest="max", default="100", help="Max value for 2D-List")
-parser.add_argument("--len", dest="inner", default="5", help="Inner list length")
+parser.add_argument("--max", dest="max", default="10", help="Max value for 2D-List")
+parser.add_argument("--len", dest="inner", default="2", help="Inner list length")
 parser.add_argument("--append", dest="append", default="w", help="Append to new line\nDefault is 'w' | Write and overwrite data if data exists | 'a' for append")
 parser.add_argument("--file", dest="file", default="list", help="File name for 2D-List")
 args = parser.parse_args()
@@ -24,6 +24,8 @@ def create_list():
     for outer in range(1, math.ceil(max/length)+1):
         data.append([])
         for _ in range(1, length+1):
+            if count > max:
+                break
             data[outer-1].append(count)
             count += 1
     try:
