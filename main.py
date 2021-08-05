b8ea46a6dede0cdeddee200c85f4274ef433bbb8
mufHaQ <masbrowmess33@gmail.com>
2021-08-05 18:39:08 +0700

    update: change default argument

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import math, argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--action", dest="action", default="create", help="'create' for create new list | 'print' for print list")
+parser.add_argument("--action", dest="action", default="write", help="'create' for create new list | 'print' for print list")
 parser.add_argument("--max", dest="max", default="100", help="Max value for 2D-List")
 parser.add_argument("--length", dest="inner", default="5", help="Inner list length")
 parser.add_argument("--append", dest="append", default="w", help="Append to new line\nDefault is 'w' | Write and overwrite data if data exists | 'a' for append")
