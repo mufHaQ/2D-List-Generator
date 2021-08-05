3675a820856efd0f624a5075855c0155880c76e6
mufHaQ <masbrowmess33@gmail.com>
2021-08-05 18:43:20 +0700

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math, argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--action", dest="action", default="write", help="'create' for create new list | 'print' for print list")
 parser.add_argument("--max", dest="max", default="100", help="Max value for 2D-List")
-parser.add_argument("--length", dest="inner", default="5", help="Inner list length")
+parser.add_argument("--len", dest="inner", default="5", help="Inner list length")
 parser.add_argument("--append", dest="append", default="w", help="Append to new line\nDefault is 'w' | Write and overwrite data if data exists | 'a' for append")
 parser.add_argument("--file", dest="file", default="list", help="File name for 2D-List")
 args = parser.parse_args()
