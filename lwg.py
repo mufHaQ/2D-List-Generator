b6d587b99a9e30a9ebc5fb9253784e4339c532ee
mufHaQ <masbrowmess33@gmail.com>
2021-08-05 13:23:32 +0700

    add: more command

--- lwg.py
+++ lwg.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 
 import math, argparse
-from typing import Reversible
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--action", dest="action", default="create", help="'create' for create new list | 'print' for print list")
 parser.add_argument("--max", dest="max", default="100", help="Max value for 2D-List")
-parser.add_argument("--length", dest="inn", default="5", help="List length")
+parser.add_argument("--length", dest="inn", default="5", help="Inner list length")
 parser.add_argument("--append", dest="a", default="w", help="Append to new line\nDefault is 'w' | Write and overwrite data if data exists | 'a' for append")
 parser.add_argument("--file", dest="file", default="list", help="File name for 2D-List")
 args = parser.parse_args()
 
 
+mx = int(args.max)
+inn = int(args.inn)
+file_name = args.file + '.txt'
 
 def create_list():
-    mx = int(args.max)
-    inn = int(args.inn)
     data = []
     data_inner = []
     count = 1
     vl = 1
-    file_name = args.file + '.txt'
     for _ in range(1, math.ceil(mx/inn)+1):
         for val in range(vl, mx+1):
             if count > inn:
@@ -37,8 +36,25 @@ def create_list():
     except Exception as e:
         print("Error:", e)
 
-if args.action == "create":
-    create_list()
-elif args.action == "print":
-    with open('list.txt') as file:
-        print(file.read())
+
+def main():
+    try:
+        if args.action == "write":
+            create_list()
+        else:
+            act = ""
+            if args.action == "print":
+                act = "r"
+            elif args.action == "clear":
+                act = "w"
+
+            with open(file_name, act) as file:
+                if act == "r":
+                    print(file.read())
+                elif act == "w":
+                    file.write("")
+    except Exception as e:
+        print("Error:", e)
+
+
+main()
